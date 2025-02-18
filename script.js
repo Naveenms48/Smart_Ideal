17f4392ec90567b2431be5b60b6592365b41159c
Naveenms48 <naveenms2411@gmail.com>
2025-02-18 09:13:39 +0530

    ready

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('section').forEach(section => {
+    // When the mouse enters the section
+    section.addEventListener('mouseenter', () => {
+      section.classList.add('activeness');
+    });
+
+    // When the mouse leaves the section
+    section.addEventListener('mouseleave', () => {
+      section.classList.remove('activeness');
+    });
+
+    // When the section is focused (keyboard interaction)
+    section.addEventListener('focusin', () => {
+      section.classList.add('activeness');
+    });
+
+    // When the section loses focus (keyboard interaction)
+    section.addEventListener('focusout', () => {
+      section.classList.remove('activeness');
+    });
+
+});
+
+var menu = document.querySelector('.menu-btn');
+var list_items = document.querySelector('.list-items');
+
+menu.addEventListener('click', () => {
+  list_items.classList.toggle('active')
+})
